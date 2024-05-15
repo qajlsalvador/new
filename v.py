ebb1606272ec267d7ef7b2e0ec964a29c233cca0
Andreu Salvador <119837660+qajlsalvador@users.noreply.github.com>
2024-05-16 00:15:39 +0800

    Update v.py

--- v.py
+++ v.py
@@ -44,4 +44,21 @@ st.markdown(
 uploaded_image = st.file_uploader("Upload an image of a vegetable", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
-    st.image(uploaded_image, caption='Uploaded Image', use_c
+    st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+    
+    try:
+        image = Image.open(uploaded_image)
+        image = image.resize((224, 224))
+        image = np.expand_dims(image, axis=0)
+        image = np.array(image)
+
+        pred_probabilities = model.predict(image)
+        pred_class_index = np.argmax(pred_probabilities, axis=1)[0]
+
+        if pred_class_index in vegetable_names:
+            predicted_vegetable = vegetable_names[pred_class_index]
+            st.success(f"Prediction: {predicted_vegetable}")
+        else:
+            st.warning("Unknown Vegetable")
+    except Exception as e:
+        st.warning("Error processing image. Please upload a valid image.")
