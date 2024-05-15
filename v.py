e27992d543294a0bc9324f1c0df53ca636866b97
Andreu Salvador <119837660+qajlsalvador@users.noreply.github.com>
2024-05-16 00:08:53 +0800

    Create v.py

---
+++ v.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from keras.models import load_model
+
+
+model = load_model('Vege2.h5')
+
+
+vegetable_names = {
+    0: 'Cabbage', 
+    1: 'Carrot', 
+    2: 'Eggplant', 
+    3: 'Lettuce', 
+    4: 'Onion'
+}
+
+
+st.title('Garden Vegetable Identifier')
+
+
+st.write("This tool identifies vegetables commonly found in the Garden.")
+
+
+st.write("The available vegetables are:")
+for idx, veg_name in vegetable_names.items():
+    st.write(f"- {veg_name}")
+
+uploaded_image = st.file_uploader("Upload an image of a vegetable", type=["jpg", "jpeg", "png"])
+
+
+if uploaded_image is not None:
+    st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+    
+    try:
+
+        image = Image.open(uploaded_image)
+        image = image.resize((224, 224))
+        image = np.expand_dims(image, axis=0)
+        image = np.array(image)
+        
+
+        pred_probabilities = model.predict(image)
+        pred_class_index = np.argmax(pred_probabilities, axis=1)[0]
+        
+
+        if pred_class_index in vegetable_names:
+            predicted_vegetable = vegetable_names[pred_class_index]
+            st.success(f"Prediction: {predicted_vegetable}")
+        else:
+            st.warning("Unknown Vegetable")
+    except Exception as e:
+        st.warning("Error processing image. Please upload a valid image.")
